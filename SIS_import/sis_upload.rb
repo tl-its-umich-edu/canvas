dab63ebdd33c1a83a5e2558c9a30e33ddc8e0003
Zhen Qian <zqian@umich.edu>
2014-08-18 16:40:03 -0400

    output meaningful error messages for error cases:
    1. no token file
    2. no current working directory
    3. no archive folder
    4. no logs folder
    5. invalid token value
    6 ...

--- SIS_import/sis_upload.rb
+++ SIS_import/sis_upload.rb
@@ -5,96 +5,86 @@ require "fileutils"
 
 
 def upload_to_canvas(fileName, token, server, outputDirectory)
-  # upload start time
-  p "upload start time : " + Time.new.inspect
 
-  # get file names
-  currentFileBaseName = File.basename(fileName)
-  currentFileBaseNameWithoutExtension = File.basename(fileName, ".zip")
+	# get file names
+	currentFileBaseName = File.basename(fileName)
+	currentFileBaseNameWithoutExtension = File.basename(fileName, ".zip")
 
-  #open the output file
-  begin
-    outputFile = File.open(outputDirectory + currentFileBaseNameWithoutExtension + ".txt", "w")
 
-    # Web Service call
-    p fileName
-    p server
+	#set the error flag, default to be false
+	upload_error = ""
 
-    json_data=`curl -H "Content-Type: application/zip" --data-binary @#{fileName} -H "Authorization: Bearer #{token}" #{server}/api/v1/accounts/1/sis_imports.json?import_type=instructure_csv`
-    
-    outputFile.write("#{json_data}\n")
-    parsed = JSON.parse(json_data)
-    
-    if (parsed["errors"])
-        ## break and print error
-        error_array=parsed["errors"]
-        ## hashmap ["message"=>"error_message"
-        p "upload error: " + error_array[0]["message"]
-        outputFile.write("upload error: " + error_array[0]["message"])
-        outputFile.write("\n")
-        
-        ##set the error flag to be true
-        upload_error = true
-        
-        return upload_error
-    end
+	#open the output file
+	begin
+		outputFile = File.open(outputDirectory + currentFileBaseNameWithoutExtension + ".txt", "w")
 
-    job_id=parsed["id"]
+		# Web Service call
+		p fileName
+		p server
 
-    outputFile.write("the job id is: #{job_id}\n")
-    outputFile.write("here is the job #{job_id} status: \n")
+		json_data=`curl -H "Content-Type: application/zip" --data-binary @#{fileName} -H "Authorization: Bearer #{token}" #{server}/api/v1/accounts/1/sis_imports.json?import_type=instructure_csv`
 
-    #set the error flag, default to be false
-    upload_error = false
+		outputFile.write("#{json_data}\n")
+		parsed = JSON.parse(json_data)
 
-    begin
-      #sleep every 10 sec, before checking the status again
-      sleep(10);
+		if (parsed["errors"])
+			## break and print error
+			error_array=parsed["errors"]
+			## hashmap ["message"=>"error_message"
+			uploadError = error_array[0]["message"]
+			outputFile.write("upload error: " + uploadError)
+			outputFile.write("\n")
 
-      json_result=`curl '#{server}/api/v1/accounts/1/sis_imports/#{job_id}' -H "Authorization: Bearer #{token}"`
+			return uploadError
+		end
 
-      #print out the whole json result
-      outputFile.write("#{json_result}\n")
+		job_id=parsed["id"]
 
-      #parse the status percentage
-      parsed_result=JSON.parse(json_result)
-      if (parsed_result["errors"])
-        ## break and print error
-        error_array=parsed_result["errors"]
-        ## hashmap ["message"=>"error_message"
-        p "upload error: " + error_array[0]["message"]
-        outputFile.write("upload error: " + error_array[0]["message"])
-        outputFile.write("\n")
+		outputFile.write("the job id is: #{job_id}\n")
+		outputFile.write("here is the job #{job_id} status: \n")
 
-        ##set the error flag to be true
-        upload_error = true
-        
-        break
-      else
-        job_progress=parsed_result["progress"]
-        outputFile.write("processed #{job_progress}\n")
-      end
-    end until job_progress == 100
+		begin
+			#sleep every 10 sec, before checking the status again
+			sleep(10);
 
-    if (!upload_error)
-      # print out the process warning, if any
-      if (parsed_result["processing_errors"])
-        outputFile.write("upload process errors: #{parsed_result["processing_errors"]}\n")
-      elsif (parsed_result["processing_warnings"])
-        outputFile.write("upload process warning: #{parsed_result["processing_warnings"]}\n")
-      else
-        outputFile.write("upload process finished successfully\n")
-      end
-    end
-  # close output file
-  ensure
-    outputFile.close unless outputFile == nil
-  end
+			json_result=`curl '#{server}/api/v1/accounts/1/sis_imports/#{job_id}' -H "Authorization: Bearer #{token}"`
 
-  # upload stop time
-  p "upload stop time : " + Time.new.inspect
+			#print out the whole json result
+			outputFile.write("#{json_result}\n")
+
+			#parse the status percentage
+			parsed_result=JSON.parse(json_result)
+			if (parsed_result["errors"])
+				## break and print error
+				uploadError=parsed_result["errors"]
+				## hashmap ["message"=>"error_message"
+				outputFile.write("upload error: " + uploadError)
+				outputFile.write("\n")
+
+				break
+			else
+				job_progress=parsed_result["progress"]
+				outputFile.write("processed #{job_progress}\n")
+			end
+		end until job_progress == 100
+
+		if (!upload_error)
+			# print out the process warning, if any
+			if (parsed_result["processing_errors"])
+				outputFile.write("upload process errors: #{parsed_result["processing_errors"]}\n")
+			elsif (parsed_result["processing_warnings"])
+				outputFile.write("upload process warning: #{parsed_result["processing_warnings"]}\n")
+			else
+				outputFile.write("upload process finished successfully\n")
+			end
+		end
+
+	# close output file
+	ensure
+	outputFile.close unless outputFile == nil
+	end
   
-  return upload_error
+	return upload_error
 
 end ## end of method definition
 
@@ -112,26 +102,30 @@ currentDirectory=""
 count=1
 # iterate through the inline arguments
 ARGV.each do|arg|
-  if (count==1)
-    File.open(arg, 'r') do |tokenFile|
-      while line = tokenFile.gets
-        # only read the first line, which is the token value
-        token=line.strip
-        break
-      end
-    end
-  elsif (count==2)
-    # the second argument should be the server name
-    server=arg
-  elsif (count==3)
-    # the third path should be the workspace path
-    currentDirectory=arg
-  else
-   # break
-  end
+	if (count==1)
+		if (Dir[arg].length != 1)
+			## token file
+			abort("Cannot find token file " + arg)
+		end
+		File.open(arg, 'r') do |tokenFile|
+			while line = tokenFile.gets
+				# only read the first line, which is the token value
+				token=line.strip
+				break
+			end
+		end
+	elsif (count==2)
+		# the second argument should be the server name
+		server=arg
+	elsif (count==3)
+		# the third path should be the workspace path
+		currentDirectory=arg
+	else
+		# break
+	end
 
-  #increase count
-  count=count+1
+	#increase count
+	count=count+1
 end
 
 # get the current working directory and the archive folder inside
@@ -143,25 +137,45 @@ p "current directory: " + currentDirectory
 p "archive directory: " + archiveDirectory
 p "output directory: " + outputDirectory
 
-# search for the canvas import zip file
-fileNameSearch=currentDirectory+ "Canvas_Extract_*.zip"
-fileNames = Dir[fileNameSearch];
-if (fileNames.length == 1)
-  ## get the name of file to process
-  fileName=fileNames[0]
-  currentFileBaseName = File.basename(fileName)
-
-  # upload the file to canvas server
-  uploadError = upload_to_canvas(fileName, token, server, outputDirectory)
-
-  if (!uploadError)
-      ## if there is no upload error
-      # move file to archive directory after processing
-      FileUtils.mv(fileName, archiveDirectory+currentFileBaseName)
-      p "SIS upload finished with " + fileName
-      exit
-  end
+if (Dir[currentDirectory].length != 1)
+	## working directory
+	abort("Cannot find current working directory " + currentDirectory)
+elsif (Dir[archiveDirectory].length != 1)
+	## archive directory
+	abort("Cannot find archive directory " + archiveDirectory)
+elsif (Dir[outputDirectory].length != 1)
+	## logs directory
+	abort("Cannot find logs directory " + outputDirectory)
 else
-  abort("Cannot find file")
+	# the canvas import zip file
+	fileNames = Dir[currentDirectory+ "Canvas_Extract_*.zip"];
+	if (fileNames.length != 1)
+		## cannot find zip file to upload
+		abort("Cannot find SIS zip file")
+	elsif
+		## get the name of file to process
+		fileName=fileNames[0]
+		currentFileBaseName = File.basename(fileName)
+
+		# upload start time
+		p "upload start time : " + Time.new.inspect
+
+		# upload the file to canvas server
+		uploadError = upload_to_canvas(fileName, token, server, outputDirectory)
+
+
+		# upload stop time
+		p "upload stop time : " + Time.new.inspect
+
+		if (!uploadError)
+			## if there is no upload error
+			# move file to archive directory after processing
+			FileUtils.mv(fileName, archiveDirectory+currentFileBaseName)
+			p "SIS upload finished with " + fileName
+			exit
+		else
+			abort(uploadError)
+		end
+	end
 end
 
