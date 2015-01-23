1f0ac25447c6a48d18b518b4c943e88daf580a67
pushyami <pushyami@umich.edu>
2015-01-23 07:50:15 -0500

    TLUNIZIN-301- calling fillContext instead of handleRequest methode to handle velocity template

--- sectionsUtilityTool/src/main/java/edu/umich/ctools/sectionsUtilityTool/SectionsUtilityToolServlet.java
+++ sectionsUtilityTool/src/main/java/edu/umich/ctools/sectionsUtilityTool/SectionsUtilityToolServlet.java
@@ -1,5 +1,8 @@
 package edu.umich.ctools.sectionsUtilityTool;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -17,22 +20,34 @@ public class SectionsUtilityToolServlet extends VelocityViewServlet {
 	
 	
     private static final String NAME = "Hello World !!!";
-    
-    protected Template handleRequest(HttpServletRequest request,
-            HttpServletResponse response, Context ctx)
-    {
-         M_log.debug("Start of the application......");   
-    	Template template = null;
-        try {
-        	template = getTemplate("templates/sections.vm");
-        	}
-        catch (Exception e) {
-        	M_log.debug("Some weired error occured");
-        	}
-        
-        ctx.put("name", NAME);
-        return template;
+    public void init() throws ServletException {
+		M_log.debug("init: called");
     }
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+    	M_log.debug("doGET: called");
+		doRequest(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		M_log.debug("doPOST: called");
+		doRequest(request, response);
+	}
+
+	public void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		M_log.debug("doPUT: called");
+		doRequest(request, response);
+	}
+	public void fillContext(Context context, HttpServletRequest request) {
+		 M_log.debug("fillContext: called");   
+	        context.put("name", NAME);
+		
+	}
+    
+    
 
 
 }
