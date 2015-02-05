f201dd88a2fc41850f7c3eaddecaa8fec148ae24
pushyami <pushyami@umich.edu>
2015-02-05 13:39:14 -0500

    TLUNIZIN-328 wiring  UI to servlet

--- sectionsUtilityTool/src/main/java/edu/umich/ctools/sectionsUtilityTool/SectionsUtilityToolServlet.java
+++ sectionsUtilityTool/src/main/java/edu/umich/ctools/sectionsUtilityTool/SectionsUtilityToolServlet.java
@@ -1,134 +1,108 @@
 package edu.umich.ctools.sectionsUtilityTool;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Enumeration;
+import java.util.Map;
 
-import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.velocity.Template;
-import org.apache.velocity.context.Context;
-import org.apache.velocity.tools.view.VelocityViewServlet;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 
-public class SectionsUtilityToolServlet extends VelocityViewServlet {
+public class SectionsUtilityToolServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 7284813350014385613L;
 	private static Log M_log = LogFactory.getLog(SectionsUtilityToolServlet.class);
 	
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException {
+		M_log.debug("doGet: Called");
+		HttpClient client = new DefaultHttpClient();
+		String targetURI= "https://umich.test.instructure.com";
+		String queryString = request.getQueryString();
+		String servletPath = request.getServletPath();
+		String pathInfo = request.getPathInfo();
+		String url=null;
+		if(pathInfo!=null&&(pathInfo.contains("courses")||pathInfo.contains("terms")||pathInfo.contains("crosslist"))) {
+		if(queryString!=null) {
+		url= targetURI+pathInfo+"?"+queryString;
+		 //url= servletPath+"?"+queryString;
+		 }
+		else {
+			url=targetURI+pathInfo;
+			//url=servletPath;
+		}
+		System.out.println("URL: "+url);
+		HttpGet clientRequest = new HttpGet(url);
+		final ArrayList<NameValuePair> nameValues = new ArrayList<NameValuePair>();
+	    nameValues.add(new BasicNameValuePair("Authorization", "Bearer"+ " " +getCanvasAdminToken()));
+	    nameValues.add(new BasicNameValuePair("content-type", "application/json"));
+	    for (final NameValuePair h : nameValues)
+	    {
+	        clientRequest.addHeader(h.getName(), h.getValue());
+	    }
+		BufferedReader rd = null;
+		try {
+			 rd = new BufferedReader(new InputStreamReader(client.execute(clientRequest).getEntity().getContent()));
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String line = "";
+		StringBuilder sb = new StringBuilder();
+		while ((line = rd.readLine()) != null) {
+			sb.append(line);
+			System.out.println(sb.toString());
+		}
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		out.print(sb.toString());
+		out.flush();
+		
+		}
+		 //TODO Delete
+		String contextPath = request.getContextPath();
+		String requestURI = request.getRequestURI();
+		Enumeration headerNames = request.getHeaderNames();
+		Map<String,String> parameterMap = request.getParameterMap();
+		Enumeration<String> parameterNames = request.getParameterNames();
+		String pathTranslated = request.getPathTranslated();
+		String remoteAddr = request.getRemoteAddr();
+		String localAddr = request.getLocalAddr();
+		StringBuffer requestURL = request.getRequestURL();
+		String serverName = request.getServerName();
+		System.out.println("ServerName: "+serverName);
+		System.out.println("ServletPath: "+servletPath);
+		System.out.println("RequestURL: "+requestURL.toString());
+		System.out.println("Remote Address: "+remoteAddr);
+		System.out.println("PathTranslated: "+pathTranslated);
+		System.out.println("LocalAddr: "+localAddr);
+		System.out.println("PathInfo: "+pathInfo);
+		System.out.println("ParameterNames: "+parameterNames);
+		System.out.println("ParameterMap: "+parameterMap);
+		System.out.println("HeaderNames: "+headerNames);
+		System.out.println("RequestURI: "+requestURI);
+		System.out.println("QueryString: "+queryString);
+		System.out.println("ContextPath: "+contextPath);
+	}
 	
-    private static final String NAME = "Hello World !!!";
-    public void init() throws ServletException {
-    	M_log.debug("##################################################################################################");
-		M_log.debug("init: called");
-    }
-    
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-    	M_log.debug("doGET: called");
-		doRequest(request, response);
+	private String getCanvasAdminToken() {
+		return "REPLACE WITH ACTUAL TOKEN";
 	}
-
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		M_log.debug("doPOST: called");
-		doRequest(request, response);
-	}
-
-	public void doPut(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		M_log.debug("doPUT: called");
-		doRequest(request, response);
-	}
-	public void fillContext(Context context, HttpServletRequest request) {
-		 M_log.debug("fillContext: called");   
-	        String uniqueName = request.getParameter("uniquename");
-	        String termList = request.getParameter("term");
-	        String primaryInstructorForm = request.getParameter("form1");
-	        context.put("name", NAME);
-	        if (primaryInstructorForm!=null) {
-	        String[] split = primaryInstructorForm.split(",");
-	        HashMap<String, List<String>> coursesOfInstructorForATerm = getCoursesOfInstructorForATerm();
-	       // context.put("uniquename", split[1]);
-	        //context.put("termList", split[0]);
-	        context.put("coursesOfInstructor", coursesOfInstructorForATerm);
-	        }
-	}
-
-	private HashMap<String,List<String>> getCoursesOfInstructorForATerm() {
-		 M_log.debug("getCoursesOfInstructorForATerm(): called");   
-		
-		HashMap<String, List<String>> course=new HashMap<String, List<String>>();
-		List <String> physics101Sections=new ArrayList<String>();
-		physics101Sections.add("Physics 101 Sec A");
-		physics101Sections.add("Physics 101 Sec B");
-		physics101Sections.add("Physics 101 Sec C");
-		physics101Sections.add("Physics 101 Sec D");
-		physics101Sections.add("Physics 101 Sec E");
-		
-		List <String> physics102Sections=new ArrayList<String>();
-		physics102Sections.add("Physics 102 Sec A");
-		physics102Sections.add("Physics 102 Sec B");
-		physics102Sections.add("Physics 102 Sec C");
-		physics102Sections.add("Physics 102 Sec D");
-		physics102Sections.add("Physics 102 Sec E");
-		
-		List <String> physics103Sections=new ArrayList<String>();
-		physics103Sections.add("Physics 103 Sec A");
-		physics103Sections.add("Physics 103 Sec B");
-		physics103Sections.add("Physics 103 Sec C");
-		physics103Sections.add("Physics 103 Sec D");
-		physics103Sections.add("Physics 103 Sec E");
-		
-		List <String> physics104Sections=new ArrayList<String>();
-		physics104Sections.add("Physics 104 Sec A");
-		physics104Sections.add("Physics 104 Sec B");
-		physics104Sections.add("Physics 104 Sec C");
-		physics104Sections.add("Physics 104 Sec D");
-		physics104Sections.add("Physics 104 Sec E");
-		
-		List <String> physics105Sections=new ArrayList<String>();
-		physics105Sections.add("Physics 105 Sec A");
-		physics105Sections.add("Physics 105 Sec B");
-		physics105Sections.add("Physics 105 Sec C");
-		physics105Sections.add("Physics 105 Sec D");
-		physics105Sections.add("Physics 105 Sec E");
-		
-		List <String> physics106Sections=new ArrayList<String>();
-		physics106Sections.add("Physics 106 Sec A");
-		physics106Sections.add("Physics 106 Sec B");
-		physics106Sections.add("Physics 106 Sec C");
-		physics106Sections.add("Physics 106 Sec D");
-		physics106Sections.add("Physics 106 Sec E");
-		
-		course.put("Physics 101", physics101Sections);
-		course.put("Physics 102", physics102Sections);
-		course.put("Physics 103", physics103Sections);
-		course.put("Physics 104", physics104Sections);
-		course.put("Physics 105", physics105Sections);
-		course.put("Physics 106", physics106Sections);
-		return course;
-	}
-
-	protected void setContentType(HttpServletRequest request,
-			HttpServletResponse response) {
-		response.setContentType("text/html; charset=UTF-8");
-	}
-	/**
-	 * TODO: come back to be more descriptive 
-	 * This fetches the admin token that can be used for Quering canvasAPI.  
-	 * @return
-	 */
-	
-	private String getAdminToken() {
-		return "ERJDKKD";
-	}
-    
     
 
 
---
+++ sectionsUtilityTool/src/main/webapp/assets/js/utils.js
@@ -0,0 +1,58 @@
+'use strict';
+/* jshint  strict: true*/
+/* global $*/
+
+
+
+/**
+ * set up global ajax options
+ */
+$.ajaxSetup({
+  type: 'GET',
+  dataType: 'json',
+  cache: false
+});
+
+
+     
+var errorHandler = function (url, result) {    
+  var errorResponse = {};    
+  if (!result) {   
+    errorResponse.message = 'Something happened!';   
+    errorResponse.requestUrl = url;    
+    errorResponse.details = result.status;   
+   
+  } else {   
+    errorResponse.message = 'Something happened with a service we depend on!';   
+    errorResponse.requestUrl = url;    
+    errorResponse.details = result.status;   
+  }    
+  return errorResponse;       
+};
+
+/**
+ *
+ * event watchers
+ */
+
+//handler for the Update Course button
+$(document).on('click', '.setSections', function (e) {
+  e.preventDefault();
+  $('#debugPanel').empty();
+  var thisCourse = $(this).attr('data-courseid');
+  var $sections = $(this).closest('li').find('ul').find('li');
+  $sections.each(function( ) {
+    //TODO: this needs to be the servlet endpoint
+    // right now just showing it in the UI
+
+    $('#debugPanel').append( '<p>POST /api/v1/sections/<strong>' + $(this).attr('data-sectionid') + '</strong>/crosslist/<strong>' + thisCourse + '</strong></p>');
+  });
+  $('#debugPanel').fadeIn('fast').delay(3000).fadeOut('slow');
+  return null;
+});
+
+$('body').on('keydown','#uniqname', function(event) {
+  if (event.keyCode == 13) {
+    $('#uniqnameTrigger').click();
+  }
+});
