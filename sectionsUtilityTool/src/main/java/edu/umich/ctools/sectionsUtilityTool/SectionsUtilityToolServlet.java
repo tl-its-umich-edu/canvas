c083443082b4f990b73991d388deb2d4359dd314
pushyami <pushyami@umich.edu>
2015-02-18 13:40:04 -0500

    TLUNIZIN_336 changes for Crosslist to happen

--- sectionsUtilityTool/src/main/java/edu/umich/ctools/sectionsUtilityTool/SectionsUtilityToolServlet.java
+++ sectionsUtilityTool/src/main/java/edu/umich/ctools/sectionsUtilityTool/SectionsUtilityToolServlet.java
@@ -17,6 +17,7 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
@@ -47,9 +48,9 @@ public class SectionsUtilityToolServlet extends HttpServlet {
 		canvasRESTAPICall(request, response);
 	}
 	
-	protected void doPOST(HttpServletRequest request,HttpServletResponse response) throws IOException {
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException {
 		M_log.debug("doPOST: Called");
-		canvasRESTAPICall(request, response);
+		canvasRESTAPICallPOST(request, response);
 		
 	}
 	
@@ -82,6 +83,59 @@ public class SectionsUtilityToolServlet extends HttpServlet {
 			url=canvasURL+pathInfo;
 		}
 		HttpGet clientRequest = new HttpGet(url);
+		final ArrayList<NameValuePair> nameValues = new ArrayList<NameValuePair>();
+	    nameValues.add(new BasicNameValuePair("Authorization", "Bearer"+ " " +canvasToken));
+	    nameValues.add(new BasicNameValuePair("content-type", "application/json"));
+	    for (final NameValuePair h : nameValues)
+	    {
+	        clientRequest.addHeader(h.getName(), h.getValue());
+	    }
+		BufferedReader rd = null;
+		try {
+			 rd = new BufferedReader(new InputStreamReader(client.execute(clientRequest).getEntity().getContent()));
+		} catch (IOException e) {
+			M_log.error("Canvas API call did not happen",e);
+		}
+		String line = "";
+		StringBuilder sb = new StringBuilder();
+		while ((line = rd.readLine()) != null) {
+			sb.append(line);
+		}
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		out.print(sb.toString());
+		out.flush();
+		
+		}
+	}
+	
+	private void canvasRESTAPICallPOST(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		request.setCharacterEncoding("UTF-8");
+		M_log.debug("canvasRESTAPICall(): called");
+		if(canvasProperties!=null) {
+			canvasToken = canvasProperties.getProperty(PROPERTY_CANVAS_ADMIN);
+			canvasURL = canvasProperties.getProperty(PROPERTY_CANVAS_URL);
+		}
+		else {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			PrintWriter out = response.getWriter();
+			out.print("Problem loading property file");
+			out.flush();
+			M_log.error("Failed to load system properties(sectionsToolProps.properties) for SectionsTool");
+			return;
+		}
+		HttpClient client = new DefaultHttpClient();
+		String queryString = request.getQueryString();
+		String pathInfo = request.getPathInfo();
+		String url=null;
+		if(pathInfo!=null&&(pathInfo.contains("courses")||pathInfo.contains("terms")||pathInfo.contains("crosslist"))) {
+		if(queryString!=null) {
+		url= canvasURL+pathInfo+"?"+queryString;
+		 }else {
+			url=canvasURL+pathInfo;
+		}
+		HttpPost clientRequest = new HttpPost(url);
 		final ArrayList<NameValuePair> nameValues = new ArrayList<NameValuePair>();
 	    nameValues.add(new BasicNameValuePair("Authorization", "Bearer"+ " " +canvasToken));
 	    nameValues.add(new BasicNameValuePair("content-type", "application/json"));
