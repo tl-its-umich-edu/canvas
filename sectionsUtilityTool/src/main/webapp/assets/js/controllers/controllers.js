8e30fc6b6d715823e9fb2b9efbfe76f51eff28ed
pushyami <pushyami@umich.edu>
2015-03-16 16:45:10 -0400

    TLUNIZIN-436 code review changes

--- sectionsUtilityTool/src/main/webapp/assets/js/controllers/controllers.js
+++ sectionsUtilityTool/src/main/webapp/assets/js/controllers/controllers.js
@@ -7,6 +7,9 @@ sectionsApp.run(function ($rootScope) {
   $rootScope.user = $.trim($('#uniqname').val());
 });
 
+function avoidCachingViaParameter(){
+	return new Date().getTime();
+};
 
 /* TERMS CONTROLLER */
 sectionsApp.controller('termsController', ['Courses', '$rootScope', '$scope', '$http', function (Courses, $rootScope, $scope, $http) {
@@ -14,7 +17,7 @@ sectionsApp.controller('termsController', ['Courses', '$rootScope', '$scope', '$
   $scope.selectedTerm = null;
   //reset term scope
   $scope.terms = [];
-  var termsUrl ='manager/api/v1/accounts/1/terms?per_page=4000&_=' + (new Date().getTime());
+  var termsUrl ='manager/api/v1/accounts/1/terms?per_page=4000&_=' + avoidCachingViaParameter();
   $http.get(termsUrl).success(function (data) {
     if(data.enrollment_terms){
       $scope.terms = data.enrollment_terms;
@@ -46,7 +49,7 @@ sectionsApp.controller('coursesController', ['Courses', 'Sections', '$rootScope'
  $scope.getCoursesForUniqname = function () {
     var uniqname = $.trim($('#uniqname').val());
     $scope.uniqname = uniqname;
-    var mini='/manager/api/v1/courses?as_user_id=sis_login_id:' +uniqname+ '&include=sections&per_page=200&published=true&with_enrollments=true&enrollment_type=teacher&_='+ (new Date().getTime());
+    var mini='/manager/api/v1/courses?as_user_id=sis_login_id:' +uniqname+ '&include=sections&per_page=200&published=true&with_enrollments=true&enrollment_type=teacher&_='+ avoidCachingViaParameter();
     var url = '/sectionsUtilityTool'+mini;
     $scope.loading = true;
     Courses.getCourses(url).then(function (result) {
