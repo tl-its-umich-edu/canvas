be2522b6004901a1aa299c3b3e3baed07a8f73d4
pushyami <pushyami@umich.edu>
2015-03-18 17:18:21 -0400

    Tl-423 better comments

--- sectionsUtilityTool/build.sh
+++ sectionsUtilityTool/build.sh
@@ -1,20 +1,19 @@
 #!/bin/sh --
 
 #Helper script for use on build server.
-#
 # Intended to be run after the Maven build is successful.
-#
 
 # Debugging: -x to enable, +x to disable
-set -x
+set +x
 
 timestamp=$(date +%Y%m%d%H%M%S)
 cd sectionsUtilityTool
 cd target
 warFilename=$(ls *.war | head -1)
 targetFilename=$(basename ${warFilename} .war)
-#origin/TLUNIZIN-424, origin/master
+#GIT_BRANCH =origin/TLUNIZIN-424 or origin/master jenkins environmental variable to get git branch
 branch=${GIT_BRANCH}
+# substring'ing  ignores string before /
 btemp=${branch:7}
 mv ${targetFilename}.war ${targetFilename}.${timestamp}.${btemp}.war
 
