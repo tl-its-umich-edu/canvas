04442d674db51e8cd9d17ee5ec91292bf8fb46a4
David Haines <dlhaines@umich.edu>
2015-10-14 11:21:42 -0400

    TLUNIZIN-767: update documentation

--- ApiTests/canvasTLAdminTest.sh
+++ ApiTests/canvasTLAdminTest.sh
@@ -1,44 +1,60 @@
 set +x
 
-### Set the token and prefix values for the different users.
-### Canvas token for a admin / masquerade canvas user.
-CANVAS_PREFIX=https://umich.test.instructure.com/api/v1
-ESB_PREFIX=https://api-qa-gw.its.umich.edu/CanvasTL/Admin/v1
+function help {
+echo "This script tests the Canvas TL API which allows canvas requests using masquerading."
+echo "Copy the AUTH.sh.template file to AUTH.sh and fill in the appropriate tokens."
+echo "The CanvasTLAdmin ESB API will allow masquarding by default.  The direct Canvas account"
+echo "must be for a user with masquerade permissions."
+echo ""
+echo "If the right credentials are added the same queries can be run through different"
+echo "access channels. E.g. they can be run via direct canvas access or"
+echo "via access through a ESB API. See script for an example."
+echo "The results of queries through different channels should be the same except:"
+echo "- A query for the user actually running the query may give different information"
+echo "as ESB API supplies it's own account for making queries."
+echo "- The accounts for the direct and ESB quries need to have the same permissions in Canvas."
+}
 
-## must contain values for CANVAS and ESB.
+### Set the token and prefix values for the different users.
 source ./AUTH.sh
 
-echo "++++++++++ testing canvas access $TYPE ++++++++++"
+help
+
+echo "++++++++++ testing canvas access $CHANNEL_NAME ++++++++++"
 
 function run_tests {
-    echo "############################# $TYPE ###################"
-    echo "+++ trying self $TYPE +++"
-    USER_ID=self
-    curl -s -H "Authorization: Bearer ${TOKEN}" $URL_PREFIX/users/${USER_ID}/profile
-    echo "----------"
+    echo "############################# $CHANNEL_NAME ###################"
+#    echo "+++ trying self $CHANNEL_NAME +++"
+#    USER_ID=self
+#    curl -s -H "Authorization: Bearer ${TOKEN}" $URL_PREFIX/users/${USER_ID}/profile
+#    echo
+#    echo "----------"
 
-    echo "+++ trying sis_login_id $TYPE +++"
-    USER_ID=sis_login_id:dlhaines
-    curl -s -H "Authorization: Bearer ${TOKEN}" $URL_PREFIX/users/${USER_ID}/profile
-    echo "----------"
+#    echo "+++ trying sis_login_id $CHANNEL_NAME +++"
+#    USER_ID=sis_login_id:dlhaines
+#    curl -s -H "Authorization: Bearer ${TOKEN}" $URL_PREFIX/users/${USER_ID}/profile
+#    echo
+#    echo "----------"
 
-    echo "+++ trying masquerading $TYPE +++"
+    echo "+++ trying masquerading $CHANNEL_NAME +++"
     USER_ID=sis_login_id:dlhaines
     curl -s -H "Authorization: Bearer ${TOKEN}" $URL_PREFIX/users/self/profile?as_user_id=sis_login_id:studenta
+    echo 
     echo "----------"
 }
 
 
 #### setup canvas direct user.
-TYPE="CANVAS DIRECT"
+CHANNEL_NAME="CANVAS DIRECT"
 TOKEN=$CANVAS_TOKEN
 URL_PREFIX=$CANVAS_PREFIX
+## comment out the canvas direct tests by default.
 
-run_tests
+#run_tests
 
 #### setup the ESB CanvasTLAdmin user.
 
-TYPE="CANVAS ESB"
+CHANNEL_NAME="CANVAS ESB"
 TOKEN=$ESB_TOKEN
 URL_PREFIX=$ESB_PREFIX
 
