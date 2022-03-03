3456268a5403a8139129a870871a820ad23c2215
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2022-03-03 14:40:26 -0500

    Switch to -slim image, switch RClone version to 1.57.0, slight update to OPTS to include config.

--- SIS_import/rclone_setup.sh
+++ SIS_import/rclone_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
-export RCLONE_OPTS=/app/config/secrets/rclone.conf --cache-dir /tmp/.cache/rclone
+RCLONE_OPTS="--config /app/config/secrets/rclone.conf --cache-dir /tmp/.cache/rclone/"
 
 # rclone --config ./rclone.conf config show
-rclone --config ${RCLONE_OPTS} lsd ~/
+rclone ${RCLONE_OPTS} lsd ~/
 
-rclone -vv --config ${RCLONE_OPTS} ls sftp:/OUTBOUND
+rclone -vv ${RCLONE_OPTS} ls sftp:/OUTBOUND
 
 # SIS upload
 ## 1.copy SIS zip file from the SFTP server
-rclone -vv --config ${RCLONE_OPTS} --exclude '.*' copy sftp:/OUTBOUND/ /usr/src/app/data
+rclone -vv ${RCLONE_OPTS} --exclude '.*' copy sftp:/OUTBOUND/ /usr/src/app/data
 
 
 ## 2. start with the sis_upload.rb
@@ -18,10 +18,10 @@ ruby /usr/src/app/sis_upload.rb
 if [ $? -eq 0 ] 
 then
     ##3. archive the SIS zip file to AWS
-    rclone copy --config ${RCLONE_OPTS} --no-traverse --exclude '.*' /usr/src/app/data aws:umich-tl-sis/archive
+    rclone copy ${RCLONE_OPTS} --no-traverse --exclude '.*' /usr/src/app/data aws:umich-tl-sis/archive
 
     ## 4. remove the SIS zip file from SFTP server
-    rclone -vv --config ${RCLONE_OPTS} delete sftp:/OUTBOUND/
+    rclone -vv ${RCLONE_OPTS} delete sftp:/OUTBOUND/
 
 else
     echo "ERROR: There was a problem in SIS upload process. Please check the details in log file. "
